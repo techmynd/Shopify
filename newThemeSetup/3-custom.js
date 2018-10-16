02e836244b7262e9d984b0b83dfb839f3def9aab
javed khalil <admin@techmynd.com>
2018-10-16 07:20:11 +0500

    update

--- newThemeSetup/3-custom.js
+++ newThemeSetup/3-custom.js
@@ -1,36 +1,56 @@
+/* ===================== */
+/* Document Ready Starts */
+/* ===================== */
+
 $(document).ready(function(){
-	// ....
   
-  //add defer if its not in script tag  
+  // add defer if its not in script tags
   $('script').each(function(){
-  	if( !$(this).attr('defer') || $(this).attr('defer', '') ) {
-       $(this).attr('defer', 'defer');
+    if( !$(this).attr('defer') || $(this).attr('defer', '') ) {
+      $(this).attr('defer', 'defer');
     }
   });
-  //end 
-  	
+  // end defer
+    
 });
 
+/* =================== */
+/* Document Ready Ends */
+/* =================== */
+
+/* ============== */
+/* On Load Starts */
+/* ============== */
 
 $(window).on('load', function(){
-	// ....
+  // ....
 });
 
+/* ============ */
+/* On Load Ends */
+/* ============ */
+
+/* ================ */
+/* On Resize Starts */
+/* ================ */
 
 $(window).on('resize', function(){
-	// ....
+  // ....
 });
 
+/* ============== */
+/* On Resize Ends */
+/* ============== */
 
-
-// FUNCTIONS
-///////////////////////////////////////////////////////
-///////////////////////////////////////////////////////
-///////////////////////////////////////////////////////
-///////////////////////////////////////////////////////
-
+/* =============== */
+/* Functions Start */
+/* =============== */
 
 // function description
 var functionName = function(){
 
 }
+
+/* ============= */
+/* Functions End */
+/* ============= */
